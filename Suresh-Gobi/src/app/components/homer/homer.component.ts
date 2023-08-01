35db30a0e9805904ee4a877682085fe3f95eb718
Suresh_Gobi <sureshgobi34@gmail.com>
2023-08-01 20:45:55 +0530

    Config

---
+++ Suresh-Gobi/src/app/components/homer/homer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homer',
+  templateUrl: './homer.component.html',
+  styleUrls: ['./homer.component.css']
+})
+export class HomerComponent {
+
+}
