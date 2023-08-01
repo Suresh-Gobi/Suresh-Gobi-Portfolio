ca7f0f55800f4bc3dab765bbc93f9b1bc301e8f9
Suresh_Gobi <sureshgobi34@gmail.com>
2023-08-01 21:01:20 +0530

    Nav

--- Suresh-Gobi/src/app/app.module.ts
+++ Suresh-Gobi/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
-import { HomerComponent } from './components/homer/homer.component';
+import { HomeComponent } from './components/homer/homer.component';
+import { mapToResolve } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomerComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Suresh-Gobi/src/app/components/header/header.component.ts
+++ Suresh-Gobi/src/app/components/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  activeSection: string | null = null;
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.highlightActiveSection();
+  }
+
+  highlightActiveSection() {
+    const sections = document.querySelectorAll('section'); // Change this selector to match your sections
+    const scrollPosition = window.pageYOffset;
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (scrollPosition >= sectionTop - 50 && scrollPosition < sectionTop + sectionHeight) {
+        this.activeSection = section.id;
+      }
+    });
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+    this.highlightActiveSection();
+  }
 }
