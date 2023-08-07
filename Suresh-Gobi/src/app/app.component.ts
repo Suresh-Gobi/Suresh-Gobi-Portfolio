8ea778ed21878b88411ef321b8c29a436b526edd
Suresh_Gobi <sureshgobi34@gmail.com>
2023-08-08 03:18:42 +0530

    New modification

--- Suresh-Gobi/src/app/app.component.ts
+++ Suresh-Gobi/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Suresh-Gobi';
+  showPreloader: boolean = true;
+
+  ngOnInit() {
+    // Simulate a minimum of 3 seconds loading time
+    setTimeout(() => {
+      this.showPreloader = false;
+    }, 2000);
+  }
 }
