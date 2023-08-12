6d74fb3ef64bc4e15937837de09a209060b58ef6
Suresh_Gobi <sureshgobi34@gmail.com>
2023-08-13 03:21:39 +0530

    Small changes!

--- Suresh-Gobi/src/app/components/homer/homer.component.ts
+++ Suresh-Gobi/src/app/components/homer/homer.component.ts
@@ -1,4 +1,15 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import projectsData from '../../../assets/projects.json';
+declare const $: any; 
+
+interface Projects {  
+  title: String;  
+  description: String; 
+  tags: string[];
+  visitLink: string;
+  githubLink: string;
+  
+}  
 
 @Component({
   selector: 'app-home',
@@ -6,6 +17,9 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./homer.component.css']
 })
 export class HomeComponent implements OnInit{
+
+  projects: Projects[] = projectsData;  
+
   activeSection: string | null = null;
 
   @HostListener('window:scroll', ['$event'])
@@ -13,8 +27,34 @@ export class HomeComponent implements OnInit{
     this.highlightActiveSection();
   }
 
+  openImageModal(): void {
+    // Clear any existing images
+    $('#imageSlideshow .carousel-inner').empty();
+
+    // Extract image URLs from the project
+    const imageUrls = [
+      'image_url_1.jpg',
+      'image_url_2.jpg',
+      'image_url_3.jpg',
+      // Add more image URLs here
+    ];
+
+    // Populate the modal with images
+    imageUrls.forEach((imageUrl, index) => {
+      const isActive = index === 0 ? 'active' : '';
+      const imageSlide = `
+        <div class="carousel-item ${isActive}">
+          <img src="${imageUrl}" class="d-block w-100" alt="Image ${index + 1}">
+        </div>`;
+      $('#imageSlideshow .carousel-inner').append(imageSlide);
+    });
+
+    // Show the modal
+    $('#imageModal').modal('show');
+  }
+
   highlightActiveSection() {
-    const sections = document.querySelectorAll('section'); // Change this selector to match your sections
+    const sections = document.querySelectorAll('section');
     const scrollPosition = window.pageYOffset;
 
     sections.forEach(section => {
